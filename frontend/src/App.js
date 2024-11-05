a620c44e8660b83bc6fa7a4d69e7af3f4aa2b0c1
dini.m <dini.mustika@dataon.com>
2024-11-05 23:11:13 +0700

    package update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { SearchContext } from "./Context/SearchContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Homepage } from "./Components/Homepage";
+import HotelList from "./Components/HotelList";
+import { FormProvider } from "./Context/FormContext";
+import Navbars from "./Components/Navbars";
+import SearchBarSmall from "./Components/SearchBarSmall";
+import MapComponent from "./Components/MapComponent";
+import Footer from "./Components/Footer";
+import DetailHotel from "./Components/Detail";
 
 function App() {
+  const value = useContext(SearchContext);
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/HotelList" element={<><Navbars/><br/><SearchBarSmall/><HotelList /><br/><Footer/></>} />
+          <Route path="/DetailHotel" element={<><Navbars/> <br/> <DetailHotel/> </>}/>
+          <Route path="/maps" element={<MapComponent/>} />
+        </Routes>
+      </BrowserRouter>
+    </FormProvider>
   );
 }
 
